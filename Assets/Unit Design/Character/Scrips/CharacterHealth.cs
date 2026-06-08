9484446f8480f0e4a476a8821986cbaaac25daea
VGHC6 <2938476516@qq.com>
2026-06-08 09:39:30 +0800

    20260608

--- Assets/Unit Design/Character/Scrips/CharacterHealth.cs
+++ Assets/Unit Design/Character/Scrips/CharacterHealth.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace UD
@@ -11,6 +12,10 @@ namespace UD
         [field: SerializeField] public float CurrentHealth { get; private set; }
         [field: SerializeField] public float MaxHealth { get; private set; }
 
+        public event Action<float> OnHealthChanged;//血量改变事件
+        public event Action OnDead;//死亡事件
+        public event Action<float> OnRevive;//复活事件
+
 
         public CharacterHealth(float currentHealth, float maxHealth)
         {
@@ -20,7 +25,17 @@ namespace UD
 
         public void ReceiveDamage(float damage)
         {
-
+            if (damage == 0) return;
+            if (damage > CurrentHealth || CurrentHealth == 0)
+            {
+                CurrentHealth = 0;
+                OnHealthChanged?.Invoke(-damage);
+                Debug.Log("You are dead");
+                OnDead?.Invoke();
+                return;
+            }
+            CurrentHealth -= damage;
+            OnHealthChanged?.Invoke(-damage);
         }
 
         public void Heal(float heal)
@@ -30,7 +45,7 @@ namespace UD
 
         public void Revive(float precent = 1f)
         {
-
+            OnRevive?.Invoke(precent);
         }
     }
 }
